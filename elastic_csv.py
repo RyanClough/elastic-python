f4efa40be558422d91281f93acb4a61c5f7d9d7f
Ryan Clough <w.ryan.clough@gmail.com>
2019-09-13 08:49:06 -0400

    ESPY-0001: initial commit

---
+++ elastic_csv.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from elasticsearch import helpers,Elasticsearch
+import csv
+
+es = Elasticsearch()
+
+index = 'baby_name_data'
+body = {
+        "mappings" : {
+            "properties" : {
+                "Id" : { "type" : "long" },
+                "Name" : { "type": "text"  },
+                "Year": { "type": "integer" },
+                "Gender": { "type": "text" },
+                "Count": { "type": "long" }
+            }
+    }
+}
+
+es.indices.create(index=index, body=body)
+es.indices.refresh()
+
+with open('NationalNames.csv') as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index='baby_name_data')
